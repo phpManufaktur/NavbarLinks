01fdf88ceb3b3c8732f670b3b2674e98f9a743df
hertsch <ralf.hertsch@phpmanufaktur.de>
2014-04-06 03:29:05 +0200

    just in progress for release 1.0.2

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-NavbarLinks
-===========
+# NavbarLinks
 
-Replacement for show_menu2() to create a Bootstrap Navbar in Templates for WebsiteBaker, LEPTON CMS or BlackCat CMS - needs the kitFramework
+Create a [Bootstrap Navbar](http://getbootstrap.com/components/#navbar) in Templates for [WebsiteBaker](http://websitebaker.org), [LEPTON CMS](http://lepton-cms.org) or [BlackCat CMS](http://blackcat-cms.org).
+
+NavbarLinks is a replacement for the default CMS function `show_menu2()` to generate menu links. 
+
+## Requirements
+
+* [WebsiteBaker](http://websitebaker.org), [LEPTON CMS](http://lepton-cms.org) or [BlackCat CMS](http://blackcat-cms.org)
+* [Bootstrap](http://getbootstrap.com)
+* [kitFramework](https://kit2.phpmanufaktur.de)
+
+## Usage
+
--- navbarlinks.php
+++ navbarlinks.php
@@ -38,186 +38,179 @@ use Symfony\Component\Translation\MessageSelector;
 use Symfony\Component\Translation\Loader\ArrayLoader;
 
 if (!defined('NAVBAR_LINKS_PATH')) {
-    define('NAVBAR_LINKS_PATH', WB_PATH.'/kit2/extension/phpmanufaktur/phpManufaktur/Library/Helper/bootstrap/navbarlinks/1.0.0');
+    define('NAVBAR_LINKS_PATH', __DIR__);
+}
+if (!defined('FRAMEWORK_PATH')) {
+    define('FRAMEWORK_PATH', WB_PATH.'/kit2');
 }
 
 require_once NAVBAR_LINKS_PATH.'/i18n/Loader/Translator.php';
 
+require_once FRAMEWORK_PATH.'/framework/twig/twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
 class bsNavbarLinks {
 
-  protected $translator = null;
-  protected static $start_level = null;
-  protected static $add_dropdown_link = null;
-  protected static $add_dropdown_divider = null;
+    protected $translator = null;
+    protected $twig = null;
+    protected static $start_level = null;
+    protected static $add_dropdown_link = null;
+    protected static $add_dropdown_divider = null;
 
-  /**
-   * Get the Page ID's for the given menu, level and visibility
-   *
-   * @param integer $menu
-   * @param integer $level
-   * @param string $visibility
-   * @throws \Exception
-   * @return Ambigous <boolean, array>
-   */
-  protected function getPageIDsForLevel($menu = 0, $level = 0, $page_id=null, $visibility = 'public')
-  {
-    global $database;
+    /**
+     * Get the Page ID's for the given menu, level and visibility
+     *
+     * @param integer $menu
+     * @param integer $level
+     * @param string $visibility
+     * @throws \Exception
+     * @return Ambigous <boolean, array>
+     */
+    protected function getPageIDsForLevel($menu = 0, $level = 0, $page_id=null, $visibility = 'public')
+    {
+        global $database;
 
-    $menu_sql = ($menu > 0) ? "AND `menu` = $menu " : '';
+        $menu_sql = ($menu > 0) ? "AND `menu` = $menu " : '';
 
-    $SQL = "SELECT `page_id` FROM `" . TABLE_PREFIX . "pages` WHERE `level`= $level $menu_sql" .
-            "AND `visibility`='$visibility'";
-    if (!is_null($page_id)) {
-        $SQL .= " AND `parent`='$page_id'";
+        $SQL = "SELECT `page_id` FROM `" . TABLE_PREFIX . "pages` WHERE `level`= $level $menu_sql" . "AND `visibility`='$visibility'";
+        if (! is_null($page_id)) {
+            $SQL .= " AND `parent`='$page_id'";
+        }
+        $SQL .= " ORDER BY `position` ASC";
+
+        $page_ids = array();
+        if (null === ($query = $database->query($SQL))) {
+            throw new Exception($database->get_error());
+        }
+
+        while (false !== ($page = $query->fetchRow(MYSQL_ASSOC))) {
+            $page_ids[] = $page['page_id'];
+        }
+
+        return (! empty($page_ids)) ? $page_ids : false;
     }
-    $SQL .= " ORDER BY `position` ASC";
 
-    $page_ids = array();
-    if (null === ($query = $database->query($SQL))) {
-      throw new Exception($database->get_error());
+    /**
+     * Check if the given PAGE ID has a child
+     *
+     * @param integer $page_id
+     * @throws \Exception
+     * @return boolean
+     */
+    protected function PageHasChild($page_id)
+    {
+        global $database;
+
+        $SQL = "SELECT `page_id` FROM `" . TABLE_PREFIX . "pages` WHERE `parent` = $page_id LIMIT 1";
+        $page_id = $database->get_one($SQL);
+        return ($page_id > 0);
     }
 
-    while (false !== ($page = $query->fetchRow(MYSQL_ASSOC))) {
-      $page_ids[] = $page['page_id'];
-    }
+    /**
+     * Get information about the given menu
+     *
+     * @param integer $page_id
+     * @throws Exception
+     * @return array|boolean
+     */
+    protected function getMenuInformation($page_id)
+    {
+        global $database;
 
-    return (!empty($page_ids)) ? $page_ids : false;
-  }
+        $SQL = "SELECT `parent`, `root_parent`, `position`, `level`, `menu_title`, `page_title`, `link` FROM `" . TABLE_PREFIX . "pages` WHERE `page_id`=$page_id";
+        if (null === ($query = $database->query($SQL))) {
+            throw new Exception($database->get_error());
+        }
 
-  /**
-   * Check if the given PAGE ID has a child
-   *
-   * @param integer $page_id
-   * @throws \Exception
-   * @return boolean
-   */
-  protected function PageHasChild($page_id)
-  {
-    global $database;
+        $result = $query->fetchRow(MYSQL_ASSOC);
 
-    $SQL = "SELECT `page_id` FROM `" . TABLE_PREFIX . "pages` WHERE `parent` = $page_id LIMIT 1";
-    $page_id = $database->get_one($SQL);
-    return ($page_id > 0);
-  }
+        if (isset($result['parent'])) {
+            $SQL = "SELECT MAX(`position`) FROM `" . TABLE_PREFIX . "pages` WHERE `parent`=" . $result['parent'];
+            $max_position = $database->get_one($SQL);
 
-  /**
-   * Get the URL of the submitted PAGE_ID
-   *
-   * @param integer $page_id
-   * @return boolean|string
-   */
-  public static function getURLbyPageID($page_id)
-  {
-    global $database;
-
-    $SQL = "SELECT `link` FROM `" . TABLE_PREFIX . "pages` WHERE `page_id`='$page_id'";
-    $link = $database->get_one($SQL, MYSQL_ASSOC);
-    if ($database->is_error()) {
-      trigger_error(sprintf('[%s - %s] %s', __FUNCTION__, __LINE__, $database->get_error()), E_USER_ERROR);
-      return false;
-    }
-    return WB_URL . PAGES_DIRECTORY . $link . PAGE_EXTENSION;
-  }
-
-  /**
-   * Get the page title for the given page ID
-   *
-   * @global object $database
-   * @param integer $page_id
-   * @return string
-   */
-  protected function getPageTitle($page_id)
-  {
-    global $database;
-
-    $SQL = "SELECT `page_title` FROM `" . TABLE_PREFIX . "pages` WHERE `page_id`='$page_id'";
-    return $database->get_one($SQL);
-  }
-
-  /**
-   * Get the menu title for the given Page ID
-   *
-   * @global object $database
-   * @param integer $page_id
-   * @return string
-   */
-  protected function getMenuTitle($page_id)
-  {
-    global $database;
-
-    $SQL = "SELECT `menu_title` FROM `" . TABLE_PREFIX . "pages` WHERE `page_id`='$page_id'";
-    return $database->get_one($SQL);
-  }
-
-  /**
-   * Build the navar items recursivly
-   *
-   * @param integer $menu
-   * @param integer $level
-   * @param string $visibility
-   * @param string reference $navbar
-   */
-  protected function buildNavbar($menu, $level, $visibility, $page_id=null, &$navbar = '')
-  {
-    $page_ids = $this->getPageIDsForLevel($menu, $level, $page_id, $visibility);
-    if (is_array($page_ids)) {
-        foreach ($page_ids as $page_id) {
-          if ($this->PageHasChild($page_id)) {
-            if ($level == self::$start_level) {
-              $navbar .= '<li class="menu-item dropdown">';
-              $navbar .= sprintf('<a href="%s" class="dropdown-toggle" data-toggle="dropdown" title="%s">%s <b class="caret"></b></a>',
-                  $this->getURLbyPageID($page_id),
-                  $this->translator->trans('Click to toggle dropdown menu'),
-                  $this->getMenuTitle($page_id));
-            }
-            else {
-              $navbar .= '<li class="menu-item dropdown dropdown-submenu">';
-              $navbar .= sprintf('<a href="%s" class="dropdown-toggle" data-toggle="dropdown" title="%s">%s</a>',
-                  $this->getURLbyPageID($page_id),
-                  $this->translator->trans('Click to toggle dropdown menu'),
-                  $this->getMenuTitle($page_id));
-            }
-            $navbar .= '<ul class="dropdown-menu">';
-            if (self::$add_dropdown_link) {
-                // add an additional dropdown link to enable selection
-                $navbar .= sprintf('<li class="%s"><a href="%s" title="%s">%s</a></li>',
-                    ($page_id == PAGE_ID) ? 'menu-item active' : 'menu-item',
-                    $this->getURLbyPageID($page_id), $this->getPageTitle($page_id),
-                    $this->getMenuTitle($page_id));
-                if (self::$add_dropdown_divider) {
-                    // add a divider to separate the link from the others
-                    $navbar .= '<li class="divider"></li>';
-                }
-            }
-            $this->buildNavbar($menu, $level + 1, $visibility, $page_id, $navbar);
-            $navbar .= '</ul></li>';
-          }
-          else {
-            $navbar .= sprintf('<li class="%s"><a href="%s" title="%s">%s</a></li>',
-                ($page_id == PAGE_ID) ? 'menu-item active' : 'menu-item',
-                $this->getURLbyPageID($page_id), $this->getPageTitle($page_id),
-                $this->getMenuTitle($page_id));
-          }
+            $result['active'] = PAGE_ID == $page_id;
+            $result['max_position'] = $max_position;
+            $result['menu_first'] = ($result['position'] == 1);
+            $result['menu_last'] = ($result['position'] == $max_position);
+            $result['url'] = WB_URL . PAGES_DIRECTORY . $result['link'] . PAGE_EXTENSION;
+            return $result;
+        } else {
+            return false;
         }
     }
-  }
 
-  /**
-   * Add a language file to the Translator service
-   *
-   * @param string $locale_path
-   * @throws \Exception
-   */
-  protected function addLanguageFiles($locale_path)
-  {
-      // get the language files
-      if (false === ($lang_files = scandir($locale_path)))
+    /**
+     * Build the navar items recursivly
+     *
+     * @param integer $menu
+     * @param integer $level
+     * @param string $visibility
+     * @param string reference $navbar
+     */
+    protected function buildNavbar($menu, $level, $visibility, $page_id=null, &$navbar = '')
+    {
+        $page_ids = $this->getPageIDsForLevel($menu, $level, $page_id, $visibility);
+        if (is_array($page_ids)) {
+            foreach ($page_ids as $page_id) {
+                if (false !== ($info = $this->getMenuInformation($page_id))) {
+                    if ($this->PageHasChild($page_id)) {
+                        if ($level == self::$start_level) {
+                            $navbar .= $this->twig->render('li.dropdown.twig', array('menu' => $info));
+                            $info['hint'] = $this->translator->trans('Click to toggle dropdown menu');
+                            $info['direction'] = 'down';
+                            $navbar .= $this->twig->render('a.dropdown-toggle.twig', array(
+                                'menu' => $info
+                            ));
+                        }
+                        else {
+                            $navbar .= $this->twig->render('li.dropdown-submenu.twig', array('menu' => $info));
+                            $info['hint'] = $this->translator->trans('Click to toggle dropdown menu');
+                            $info['direction'] = 'right';
+                            $navbar .= $this->twig->render('a.dropdown-toggle.twig', array(
+                                'menu' => $info
+                            ));
+                        }
+
+                        $navbar .= $this->twig->render('ul.dropdown-menu.twig');
+                        if (self::$add_dropdown_link) {
+                            // add an additional dropdown link to enable selection
+                            $navbar .= $this->twig->render('li.menu-item.twig', array(
+                                'menu' => $info
+                            ));
+                            if (self::$add_dropdown_divider) {
+                                // add a divider to separate the link from the others
+                                $navbar .= $this->twig->render('li.divider.twig');
+                            }
+                        }
+                        $this->buildNavbar($menu, $level + 1, $visibility, $page_id, $navbar);
+                        $navbar .= $this->twig->render('ul.li.close.twig');
+                    }
+                    else {
+                        $navbar .= $this->twig->render('li.menu-item.twig', array(
+                            'menu' => $info
+                        ));
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Add a language file to the Translator service
+     *
+     * @param string $locale_path
+     * @throws \Exception
+     */
+    protected function addLanguageFiles($locale_path)
+    {
+        // get the language files
+        if (false === ($lang_files = scandir($locale_path)))
           throw new \Exception(sprintf("Can't read the /i18n directory %s!", $locale_path));
 
-      $ignore = array('.', '..', 'index.php', 'README.md');
+        $ignore = array('.', '..', 'index.php', 'README.md');
 
-      // loop through the language files
-      foreach ($lang_files as $lang_file) {
+        // loop through the language files
+        foreach ($lang_files as $lang_file) {
           if (!is_file($locale_path.'/'.$lang_file)) {
               continue;
           }
@@ -231,16 +224,16 @@ class bsNavbarLinks {
 
           // add the locale resource file
           $this->translator->addResource('array', $lang_array, $lang_name);
-      }
-  }
+        }
+    }
 
-  /**
-   * Initialize the Symfony Translator Service
-   *
-   * @throws \Exception
-   */
-  protected function initializeTranslator()
-  {
+    /**
+     * Initialize the Symfony Translator Service
+     *
+     * @throws \Exception
+     */
+    protected function initializeTranslator()
+    {
       $this->translator = new Translator('en_EN', new MessageSelector());
       $this->translator->setFallbackLocale('en');
       $this->translator->addLoader('array', new ArrayLoader());
@@ -253,35 +246,56 @@ class bsNavbarLinks {
 
       // set the locale from the CMS
       $this->translator->setLocale(strtolower(LANGUAGE));
-  }
-
-  /**
-   * Controller to create the Navbar links
-   *
-   * @param integer $menu
-   * @param integer $level
-   * @param boolean $add_dropdown_link
-   * @param boolean $add_dropdown_divider
-   * @param string $visibility
-   * @return string
-   */
-  public function getNavbarLinks($menu=0, $level=0, $add_dropdown_link=false, $add_dropdown_divider=true, $visibility='public')
-  {
-    self::$start_level = $level;
-    self::$add_dropdown_link = $add_dropdown_link;
-    self::$add_dropdown_divider = $add_dropdown_divider;
-
-    // initialize the translation service
-    $this->initializeTranslator();
-
-    $navbar = '';
-    $this->buildNavbar($menu, $level, $visibility, null, $navbar);
-
-    if (!empty($navbar)) {
-      $navbar = sprintf('<ul class="nav navbar-nav">%s</ul>', $navbar);
     }
 
-    return $navbar;
-  }
+    /**
+     * Initialize the Twig template engine
+     */
+    protected function initializeTwig()
+    {
+        $loader = new Twig_Loader_Filesystem(array(
+          NAVBAR_LINKS_PATH.'/template/custom',
+          NAVBAR_LINKS_PATH.'/template/default'
+        ));
+
+        $this->twig = new Twig_Environment($loader, array(
+            'cache' => FRAMEWORK_PATH.'/temp/cache',
+            'strict_variables' => true,
+            'debug' => true,
+            'autoescape' => false
+        ));
+    }
+
+    /**
+     * Controller to create the Navbar links
+     *
+     * @param integer $menu
+     * @param integer $level
+     * @param boolean $add_dropdown_link
+     * @param boolean $add_dropdown_divider
+     * @param string $visibility
+     * @return string
+     */
+    public function getNavbarLinks($menu=0, $level=0, $add_dropdown_link=false, $add_dropdown_divider=true, $visibility='public')
+    {
+        self::$start_level = $level;
+        self::$add_dropdown_link = $add_dropdown_link;
+        self::$add_dropdown_divider = $add_dropdown_divider;
+
+        // initialize the translation service
+        $this->initializeTranslator();
+
+        // initialize Twig
+        $this->initializeTwig();
+
+        $navbar = '';
+        $this->buildNavbar($menu, $level, $visibility, null, $navbar);
+
+        if (!empty($navbar)) {
+            $navbar = $this->twig->render('ul.navbar.twig', array('navbar' => $navbar));
+        }
+
+        return $navbar;
+    }
 
 }
